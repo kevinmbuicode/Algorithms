b26809db2f9ed341e6779aaf806873ba209eb135
kevinmbuicode <kevinmushan@gmail.com>
2023-01-14 11:47:58 +0300

    feat: selection sort

---
+++ Algorithms Sorting/selectionSort.js
@@ -0,0 +1,21 @@
+// In this case, we select the smallest to start at the index 0 as we go on adding the rest to a sorted array;
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function selectionSort(array) {
+  const length = array.length;
+  for(let i = 0; i < length; i++){
+    // set current index as minimum
+    let min = i;
+    let temp = array[i];
+    for(let j = i+1; j < length; j++){
+      if (array[j] < array[min]){
+        //update minimum if current is lower that what we had previously
+        min = j;
+      }
+    }
+    array[i] = array[min];
+    array[min] = temp;
+  }
+  return array;
+}
