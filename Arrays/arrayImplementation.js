c1147de271a60aaef8da957ed72a46c968eff634
kevinmbuicode <kevinmushan@gmail.com>
2023-01-27 00:23:31 +0300

    chore: add push and pop

--- Arrays/arrayImplementation.js
+++ Arrays/arrayImplementation.js
@@ -1,15 +1,37 @@
 // Implementing an array from scratch.
 
-class MyArray{
-    constructor(){
+class MyArray {
+    constructor() {
         this.length = 0;
-        this.data = {}
-    };
+        this.data = {};
+    }
 
     get(index) {
         return this.data[index];
     }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.data;
+    }
+
+    pop() {
+        const lastItem = this.data[this.length - 1]
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index) {
+        this.shiftItems(this.data[index])
+    }
+
 }
 
 const newArray = new MyArray();
+newArray.push("Hi");
+newArray.push("Kev");
+newArray.push("Mbui");
+newArray.pop();
 console.log(newArray)
